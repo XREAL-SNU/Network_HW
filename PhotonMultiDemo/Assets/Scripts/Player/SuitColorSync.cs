5dc855cbc6d7bd46e38ac89c79c18abe704d655e
DESKTOP-CUVVP4G\user <khjune29@gmail.com>
2022-03-18 19:35:29 +0900

    [feat] Color of suit synchronization: Press R, G, B button to change Color

--- PhotonMultiDemo/Assets/Scripts/Player/SuitColorSync.cs
+++ PhotonMultiDemo/Assets/Scripts/Player/SuitColorSync.cs
@@ -8,7 +8,7 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 public class SuitColorSync : MonoBehaviourPunCallbacks
 {
     const string SUIT_COLOR_KEY = "SuitColor";
-    enum ColorEnum
+    public enum ColorEnum
     {
         Red, Green, Blue
     }
@@ -33,17 +33,25 @@ public class SuitColorSync : MonoBehaviourPunCallbacks
     {
         PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { SUIT_COLOR_KEY, ColorEnum.Blue } });
         block = new MaterialPropertyBlock();
-        block.SetColor("_Color", Color.black);
-        suitRenderer = transform.Find("Space_Suit/Man_Suit/Body").GetComponent<Renderer>();
-
+        suitRenderer = transform.Find("Space_Suit/Tpose_/Man_Suit/Body").GetComponent<Renderer>();
     }
 
     public override void OnPlayerPropertiesUpdate(Player player, Hashtable updatedProps)
     {
-        Debug.Log($"player {player.NickName}'s color updated to {updatedProps[SUIT_COLOR_KEY]}");
-        Debug.Log("parsed enum : " + (ColorEnum)updatedProps[SUIT_COLOR_KEY]);
         block.SetColor("_Color", GetColor((ColorEnum)updatedProps[SUIT_COLOR_KEY]));
         suitRenderer.SetPropertyBlock(block);
-
     }
+
+    public void SetColorProperty(ColorEnum col)
+    {
+        PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { SUIT_COLOR_KEY, col } });
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.R)) SetColorProperty(ColorEnum.Red);
+        if (Input.GetKey(KeyCode.G)) SetColorProperty(ColorEnum.Green);
+        if (Input.GetKey(KeyCode.B)) SetColorProperty(ColorEnum.Blue);
+    }
+
 }
